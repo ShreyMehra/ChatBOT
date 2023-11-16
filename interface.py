185a02413919b844077bbc53e3c10b7c677a9f9e
ShreyMehra <67088904+ShreyMehra@users.noreply.github.com>
2023-11-16 12:44:15 +0530

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import google.generativeai as palm
+
+palm.configure(api_key = os.getenv("api_key"))
+model_id="models/chat-bison-001"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    intro = "Hi! How are doing buddy? How can I help you?"
+    
+    st.session_state.messages.append({"ROLE" : "ASSISTANT", "CONTENT" : intro})
+    
+    
+for message in st.session_state.messages:
+    with st.chat_message(message.get('ROLE')):
+        st.write(message.get("CONTENT"))
+
+prompt = st.chat_input("Ask Anything")
+    
+if prompt:
+    st.session_state.messages.append({"ROLE" : "USER", "CONTENT" : prompt})
+
+    with st.chat_message("USER"):
+        st.write(prompt)
+    
+    response = palm.chat(messages=prompt)  
+    reply = response.last
+    st.session_state.messages.append({"ROLE" : "ASSISTANT", "CONTENT" : reply})
+
+    with st.chat_message("ASSISTANT"):
+        st.write(reply)
